0aa24d547844c9f74f8234b2736b2a8a94cd2848
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-14 10:19:41 +0100

    Added tests

--- src/main/java/q/jv/HospitalManagementApplication.java
+++ src/main/java/q/jv/HospitalManagementApplication.java
@@ -1,17 +1,43 @@
 package q.jv;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class HospitalManagementApplication {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import q.jv.hospitalmanagement.entities.Patient;
+import q.jv.hospitalmanagement.repository.PatientRepository;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Date;
+
+@SpringBootApplication
+public class HospitalManagementApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Patient patient = new Patient();
+        patient.setId(null);
+        patient.setName("Youssef");
+        patient.setDateOfBirth(new Date());
+        patient.setSick(true);
+        patient.setScore(12);
+
+        patientRepository.save(patient);
+
+        patientRepository.save(new Patient(null,"Mohamed",new Date(),false,25));
+
+        Patient patient3 = Patient.builder()
+                .name("Ali")
+                .dateOfBirth(new Date())
+                .sick(true)
+                .score(10)
+                .build();
+        patientRepository.save(patient3);
+
     }
 }
