456bd5597f06f20722659f8074aa40fdf93f041d
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-14 09:56:01 +0100

    Added the patient repository

---
+++ src/main/java/q/jv/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package q.jv.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import q.jv.hospitalmanagement.entities.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNameContains(String keyword, Pageable pageable);
+}
