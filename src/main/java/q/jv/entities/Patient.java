3d9ee1b75f803ccff5bba95657f1aa6aeac0d594
ysfelhamri <youssef.elhamri.2002@gmail.com>
2025-05-14 09:48:35 +0100

    Added the Patient entity

---
+++ src/main/java/q/jv/entities/Patient.java
@@ -0,0 +1,20 @@
+package q.jv.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.Date;
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Date dateOfBirth;
+    private boolean sick;
+    private int score;
+
+}
